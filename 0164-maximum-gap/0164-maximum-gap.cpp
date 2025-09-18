77c2a89975419df7568b32d64a7619c9648ba842
Yash Garg <72246420+yashgarg7302@users.noreply.github.com>
2025-09-18 17:52:18 +0530

    Time: 17 ms (86.83%), Space: 94.1 MB (28.6%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.cpp
+++ 0164-maximum-gap/0164-maximum-gap.cpp
@@ -1,14 +1,40 @@
 class Solution {
 public:
     int maximumGap(vector<int>& nums) {
-        int n = nums.size();
-        if(n<2) return 0;
-        int ans = 0;
-        sort(nums.begin(),nums.end());
-        for(int i=1;i<n;i++){
-            int num = nums[i] - nums[i-1];
-            ans = max(ans,num);
+    int n = nums.size();
+    if (n < 2) {
+        return 0;
+    }
+
+    int min_val = nums[0];
+    int max_val = nums[0];
+    for (int num : nums) {
+        min_val = min(min_val, num);
+        max_val = max(max_val, num);
+    }
+    
+    if (min_val == max_val) {
+        return 0;
+    }
+    int bucket_size = max(1, (max_val - min_val) / (n - 1));
+    int bucket_count = (max_val - min_val) / bucket_size + 1;
+
+    vector<pair<int, int>> buckets(bucket_count, {INT_MAX, INT_MIN});
+    
+    for (int num : nums) {
+        int bucket_index = (num - min_val) / bucket_size;
+        buckets[bucket_index].first = min(buckets[bucket_index].first, num);
+        buckets[bucket_index].second = max(buckets[bucket_index].second, num);
+    }
+    int max_gap = 0;
+    int prev_max = min_val;
+    for (const auto& bucket : buckets) {
+        if (bucket.first == INT_MAX) {
+            continue;
         }
-        return ans;
+        max_gap = max(max_gap, bucket.first - prev_max);
+        prev_max = bucket.second;
+    }
+    return max_gap;
     }
 };
